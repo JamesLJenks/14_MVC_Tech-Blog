698e4342f95f9d8ea070136b504340c9215a6788
Jay Jenks <jamesljenks@gmail.com>
2021-07-08 21:07:34 -0400

    Updated blog post routes under api folder.

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -15,6 +15,27 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const updatedPostData = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!updatedPostData) {
+      res.status(404).json({ message: 'No blog post found with this id!' });
+      return;
+    }
+
+    res.status(200).json(updatedPostData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+    
+
 router.delete('/:id', withAuth, async (req, res) => {
   try {
     const postData = await Post.destroy({
