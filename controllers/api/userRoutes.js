2051965ac3c9869478625e1639469dead2f26e8a
Jay Jenks <jamesljenks@gmail.com>
2021-07-08 21:33:47 -0400

    Added remaining code to set up user delete in userRoutes.js under the api folder.

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,6 +7,7 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.name = userData.name;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -18,7 +19,8 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { email: req.body.email } });
+    const userData = await User.findOne({ where: { name: req.body.name } 
+    });
 
     if (!userData) {
       res
@@ -38,6 +40,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
+      req.session.name = userData.name;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
@@ -57,5 +60,29 @@ router.post('/logout', (req, res) => {
     res.status(404).end();
   }
 });
+router.delete("/user/:id", (req, res) => {
+  try {
+    const deleteUser = await User.destroy({ where: { user_id: req.body.user_id } 
+    });
 
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+  
+    } catch (err) {
+      res.status(400).json(err);
+    }
+})
 module.exports = router;
