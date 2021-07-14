9d84e363c30aae8d2836add6bbdbd8def50dd610
Jay Jenks <jamesljenks@gmail.com>
2021-07-13 22:25:53 -0400

    Updated login.js and logout.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.name = userData.name;
+      req.session.username = userData.username;
       req.session.logged_in = true;
 
       res.status(200).json(userData);
@@ -19,7 +19,7 @@ router.post('/', async (req, res) => {
 
 router.post('/login', async (req, res) => {
   try {
-    const userData = await User.findOne({ where: { name: req.body.name } 
+    const userData = await User.findOne({ where: { username: req.body.username } 
     });
 
     if (!userData) {
@@ -40,7 +40,7 @@ router.post('/login', async (req, res) => {
 
     req.session.save(() => {
       req.session.user_id = userData.id;
-      req.session.name = userData.name;
+      req.session.username = userData.username;
       req.session.logged_in = true;
       
       res.json({ user: userData, message: 'You are now logged in!' });
